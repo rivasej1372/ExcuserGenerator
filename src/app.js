8e7a1daebe2de988bd2c0f63021fb5e2c4398d08
rivasej1372 <rivasej@gmail.com>
2021-09-26 17:42:51 +0000

    Project Excuse Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const excuse = {
+  who: ["The dog", "My grandma", "His turtle", "My bird"],
+  action: ["ate", "peed", "crushed", "broke"],
+  what: ["my homework", "the keys", "the car"],
+  when: [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ]
 };
+
+window.onload = () => {
+  document.querySelector("#Demo").innerHTML = excuseGenerator();
+};
+
+function excuseGenerator() {
+  return (
+    excuse.who[Math.floor(Math.random() * excuse.who.length)] +
+    " " +
+    excuse.action[Math.floor(Math.random() * excuse.action.length)] +
+    " " +
+    excuse.what[Math.floor(Math.random() * excuse.what.length)] +
+    " " +
+    excuse.when[Math.floor(Math.random() * excuse.when.length)]
+  );
+}
